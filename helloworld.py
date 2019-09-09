bde3863a995e05933a6d3e0562793c86e44bdd51
cathuang <cathuang@umich.edu>
2019-09-09 14:34:55 -0400

    wrote lab0 file

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import *
+
+print("Cathleen")
+x = randint(0, 100)
+print(x)
+y = randint(0, 100)
+print(y)
+sum = x + y
+print("Sum = ", sum)
+avg = sum / 2.0
+print("Average = ", avg)
